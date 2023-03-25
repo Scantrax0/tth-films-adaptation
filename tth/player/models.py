1870ef853318bfc95c490bd30508a3b234d93da7
Stanislav Chuprinskiy <stas.chuprinskiy@mail.ru>
2023-03-25 22:58:48 +0300

    Добавлена модель VideoEntry
    Модель зарегана в админке, содержит поля: ссылка, статус и яркостные
    данные

--- tth/player/models.py
+++ tth/player/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class VideoEntry(models.Model):
+    LOADED = 'L'
+    PROCESSING = 'P'
+    FINISHED = 'F'
+    STATUSES = [
+        (LOADED, 'Загружено'),
+        (PROCESSING, 'Обработка'),
+        (FINISHED, 'Обработано'),
+    ]
+
+    url = models.URLField('URL', unique=True, db_index=True)
+    status = models.CharField('Статус', max_length=40, choices=STATUSES,
+                              default=LOADED)
+    brightness_data = models.TextField('Данные яркости')
+
+    class Meta:
+        verbose_name = 'Видео'
+        verbose_name_plural = 'Видео'
+
+    def __str__(self):
+        return f'{self.status} | {self.url}'
