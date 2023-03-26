034b185293cb16b7dacce663d8cad73c7996417f
Stanislav Chuprinskiy <stas.chuprinskiy@mail.ru>
2023-03-26 11:02:53 +0300

    Изменил скрипт обработки яркости видео:
    - На вход VideoProcessor принимает url видео
    - Выход остался неизменным

--- processor/main.py
+++ processor/main.py
@@ -1,22 +1,18 @@
-import cv2
-import os
+from vidgear.gears import CamGear
 import math
 import json
 
-class VideoProcessor:
 
-    def __init__(self, path):
-        if os.path.isfile(path):
-            self.videopath = path
-        else:
-            raise Exception('Cannot locate the video file')
-    
+class VideoProcessor:
+    def __init__(self, youtube_url):
+        self.youtube_url = youtube_url
+
     @staticmethod
     def _perceived_brightness(frame):
-        r, g, b = frame.mean(axis=(0,1))
+        r, g, b = frame.mean(axis=(0, 1))
         return math.sqrt(0.241*(r**2) + 0.691*(g**2) + 0.068*(b**2))
-    
-    def analyze_brightness(self, SHOW=False):
+
+    def analyze_brightness(self):
         result = {
             'success': False,
             'data': {}
@@ -25,43 +21,33 @@ class VideoProcessor:
         x = []
         y = []
 
-        cap = cv2.VideoCapture(self.videopath) 
-        if not cap.isOpened():
-            raise Exception('Cannot open video file')
+        stream = CamGear(source=self.youtube_url, stream_mode=True).start()
         while True:
-            i += 1
-            ret, frame = cap.read()
-            if not ret:
-                print('Cannot receive frame, finishing analysis')
-                cap.release()
+            frame = stream.read()
+            if frame is None:
                 break
+            i += 1
             brightness = str(int(self._perceived_brightness(frame)))
             x.append(i)
             y.append(brightness)
-
-            if SHOW:
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(frame, 
-                    brightness, 
-                    (50, 50), 
-                    font, 1, 
-                    (0, 255, 255), 
-                    2, 
-                    cv2.LINE_4)
-                cv2.imshow('frame', frame)
-                cv2.waitKey(1)
         result['success'] = True
         result['data'] = {
             'total_frames': i,
             'x': x,
             'y': y,
         }
-        cap.release()
-
+        stream.stop()
         return result
 
-v = VideoProcessor("C:/projects/hackathon/tth-films-adaptation/tth/media/raw_video/SampleVideo_1280x720_2mb.mp4")
-r = v.analyze_brightness()
-with open('out.json', 'w') as f:
-    f.write(json.dumps(r.get('data')))
 
+def main():
+    url = "https://www.youtube.com/watch?v=nWBeexdXEKU"
+    v = VideoProcessor(url)
+    r = v.analyze_brightness()
+
+    with open('out.json', 'w') as f:
+        f.write(json.dumps(r.get('data')))
+
+
+if __name__ == "__main__":
+    main()
